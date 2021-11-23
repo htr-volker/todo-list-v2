9b76763536039de9195f77fdab47c2c251081ef2
NicholasGhartey <nrghartey@gmail.com>
2021-11-23 14:16:05 +0000

    Many changes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,6 @@
 from application import app, db
 from os import getenv
 
-if __name__ == "__main__":
-    if getenv("CREATE_SCHEMA").lower() == "true":
-        db.drop_all()
-        db.create_all()
+if __name__=="__main__":
+
     app.run(debug=True, host='0.0.0.0')
